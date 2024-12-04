c6b997b12fe32f93590efa1dc9795f6ef4fc66a6
Todor Genov <todor@subnet.co.za>
2024-12-04 23:36:06 +0200

    Minor improvement

--- 4b.clj
+++ 4b.clj
@@ -6,11 +6,11 @@
 
 (defn get-diagonals [grid row col]
   (let [diagonal1 (str (get-in grid [(dec row) (dec col)])
-                      (get-in grid [row col])
-                      (get-in grid [(inc row) (inc col)]))
+                       (get-in grid [row col])
+                       (get-in grid [(inc row) (inc col)]))
         diagonal2 (str (get-in grid [(dec row) (inc col)])
-                      (get-in grid [row col])
-                      (get-in grid [(inc row) (dec col)]))]
+                       (get-in grid [row col])
+                       (get-in grid [(inc row) (dec col)]))]
     [diagonal1 diagonal2]))
 
 (defn valid-diagonal? [s]
@@ -18,14 +18,14 @@
 
 (defn count-valid-as [grid]
   (count
-    (for [row (range 1 (dec (count grid)))
-          col (range 1 (dec (count (first grid))))
-          :let [[d1 d2] (get-diagonals grid row col)]
-          :when (and (= (get-in grid [row col]) \A)
-                    (valid-diagonal? d1)
+   (for [row (range 1 (dec (count grid)))
+         col (range 1 (dec (count (first grid))))
+         :when (= (get-in grid [row col]) \A)
+         :let [[d1 d2] (get-diagonals grid row col)]
+         :when (and (valid-diagonal? d1)
                     (valid-diagonal? d2))]
-      true)))
+     true)))
 
-(println 
-  (count-valid-as 
-    (read-grid "4.input")))
+(println
+ (count-valid-as
+  (read-grid "4.input")))
